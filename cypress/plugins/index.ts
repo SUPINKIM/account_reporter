636a97eba020189134e29419a5f817ced9a9ddcd
dante <dante@deali.net>
2022-01-07 15:59:38 +0900

    feat: vue3 typescript tailwindcss starter

---
+++ cypress/plugins/index.ts
@@ -0,0 +1,11 @@
+import { startDevServer } from '@cypress/vite-dev-server'
+
+module.exports = (on, config) => {
+  on('dev-server:start', options =>
+    startDevServer({
+      options,
+    })
+  )
+
+  return config
+}
